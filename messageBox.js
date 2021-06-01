c3a1fcb15e1ef172ea6d3bd68cb802a3e57a839c
Dorian Kier <dorian.kierwo@gmail.com>
2021-06-01 15:15:49 +0200

    Finished messageBox

--- messageBox.js
+++ messageBox.js
@@ -1,18 +1,19 @@
 // notification content
-var contentAlert = "Accepted Cookie?";
+var contentAlert = "Accept Cookie?";
 
 // option in message box;
-var optionOne = '<div id="optionOne" style="width: 30%; background-color: #00aa00; color: #fff; float: left; text-align: center; padding: 10px; cursor: pointer;">accepted</div>';
-var optionTwo = '<div id="optionTwo" style="width: 30%; background-color: #aa0000; color: #fff; float: right; text-align: center; padding: 10px; cursor: pointer;">dissagre</div>';
+var optionOne = '<div id="optionOne" style="width: 30%; background-color: #00aa00; color: #fff; float: left; text-align: center; padding: 10px; cursor: pointer;" onmouseover="this.style.opacity=0.7" onmouseout="this.style.opacity=1">accept</div>';
+var optionTwo = '<div id="optionTwo" style="width: 30%; background-color: #aa0000; color: #fff; float: right; text-align: center; padding: 10px; cursor: pointer;" onmouseover="this.style.opacity=0.7" onmouseout="this.style.opacity=1">dissagre</div>';
 
 // code message box
 function messageBox(){
-
     $(document).ready(function(){
+        // fix style for message box to secound open message
+        $("#messageBoxAlertJS1234").css({ display: "block" });
 
         // message box style and blur content page
-        $("#messageBoxAlertJS1234").css({ opacity: "0", filter: "blur(0)", position: "absolute", top: "35%", width: "250px", height: "90px", padding: "5px", left: "40%", border: "1px solid #000"});
-        $("main").animate({ opacity: "0.5", filter: "blur(2px)" }, 300);
+        $("#messageBoxAlertJS1234").css({ opacity: "0", filter: "blur(0)", position: "absolute", top: "35%", width: "250px", minHeight: "90px", padding: "5px", left: "40%", border: "3px double #000", borderRadius: "5px", textAlign: "center", backgroundColor: "#aaa", color: "#000"});
+        $("main").animate({ opacity: "0.5", filter: "blur(10px)" }, 300);
 
         // connected message and options
         var completeContetntBox = '<p>'+contentAlert+'</p>'+optionOne+optionTwo;
@@ -30,5 +31,4 @@ function messageBox(){
             window.location="http://google.pl";
         });
     });
-
 };
